b93d71237071e4ec14f0a19be8d0969bdd047683
Parham Tehrani <parham.ftehrani@gmail.com>
2023-08-05 17:00:16 +0330

    first commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,19 +7,18 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
               integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
         <link rel="stylesheet" href="/style.css">
-        <style>
-            /* *:focus-visible, */
-            *:focus {
-                outline: 4px dashed darkorange !important;
-            }
-        </style>
+{{--        <style>--}}
+{{--            /* *:focus-visible, */--}}
+{{--            *:focus {--}}
+{{--                outline: 4px dashed darkorange !important;--}}
+{{--            }--}}
+{{--        </style>--}}
     </head>
     <body>
 
         @yield('content')
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-        <script src="/speechRecognition.js"></script>
         <script src="/script.js"></script>
         <script>
 
